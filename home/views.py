dfa5631e8f291487f85c9c376bd236a0a1bcdf6a
Abdullah Saeed <abdullahsaeed91203@gmail.com>
2023-09-08 16:39:45 +0500

    .

--- home/views.py
+++ home/views.py
@@ -21,7 +21,7 @@ def chatAPI(request):
         messages=[
                 {"role": "user", "content": prompt}
         ],
-        temperature=0.9,
+        temperature=1,
         max_tokens=256,
         top_p=1,
         frequency_penalty=0,
